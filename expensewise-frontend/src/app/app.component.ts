b264283b771410e04a9f5ebc38af50c1b39dc13a
Navodya De Silva <kdnavodya7@gmail.com>
2025-03-17 22:32:15 +0530

    Pages Structure | Dashboard Styling

--- expensewise-frontend/src/app/app.component.ts
+++ expensewise-frontend/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common'; // Import CommonModule
 import { HeaderComponent } from "./components/header/header.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { SummaryComponent } from "./components/pages/summarypage/summary/summary.component";
+import { TransactionsComponent } from "./components/pages/transactionspage/transactions/transactions.component";
+import { InsightsComponent } from "./components/pages/insightspage/insights/insights.component";
+import { SavingsComponent } from "./components/pages/savingspage/savings/savings.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent, SummaryComponent],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, SummaryComponent, TransactionsComponent, InsightsComponent, SavingsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- expensewise-frontend/src/app/components/sidebar/sidebar.component.ts
+++ expensewise-frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';  // Import CommonModule here
+import { CommonModule } from '@angular/common';
+import { Input, Output, EventEmitter } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { AppComponent } from '../../app.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
-  standalone: true,  // Mark SidebarComponent as standalone if using standalone components
+  standalone: true, // mark sidevar as standalone component
   imports: [CommonModule]  // Import CommonModule for directives like ngClass, ngFor
 })
+
+
 export class SidebarComponent {
+  @Input() activeTab: string = 'Summary';  // Receive the activeTab from the parent component
+  @Output() activeTabChange: EventEmitter<string> = new EventEmitter<string>();  // emit changes to the parent component
+
   tabs = [
     { name: 'Summary', icon: 'home' },
     { name: 'Transactions', icon: 'account_balance_wallet' },
@@ -16,5 +25,9 @@ export class SidebarComponent {
     { name: 'Savings', icon: 'savings' }
   ];
 
-  activeTab = 'Summary';
+  // method - change the active tab
+  changeTab(tab: string) {
+    this.activeTab = tab;
+    this.activeTabChange.emit(this.activeTab);  // add the updated activeTab value to the parent
+  }
 }
